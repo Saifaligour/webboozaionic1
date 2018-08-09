14cc728b94f9edc19e7233898adc757a912bb448
Rajkishor <rajmrj1995@gmail.com>
2018-08-09 17:18:54 +0530

    added some crdova plugin

--- videocllingApp/src/pages/home/home.ts
+++ videocllingApp/src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+ alluserdata=[]
+ 
+ private userdata:FormGroup
+ 
+constructor(public navCtrl: NavController, private formBuilder: FormBuilder ) {
+    this.userdata = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: [''],
+    });
   }
 
+  logForm(){
+    console.log(this.userdata.value)
+  }
+  
 }
+
+
